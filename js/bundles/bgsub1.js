12fc7acb86f6aceaa490c3fafd4f77d9cabe51eb
angusmglfraser <alf31@aber.ac.uk>
2018-04-08 14:29:31 +0100

    Naive background subtraction

--- js/bundles/bgsub1.js
+++ js/bundles/bgsub1.js
@@ -80,11 +80,12 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var Vision = __importStar(require("../vision"));
 var RGBImage_1 = require("../RGBImage");
-var background;
+var bg;
 var animating = false;
+var threshold = 80;
 function getBackgroundFrame() {
-    var img = Vision.getImageFromVideo(document.getElementById('webcam'), document.getElementById('background'));
-    background = img;
+    var img = Vision.getImageFromVideo(document.getElementById('webcam'), document.getElementById('backgroundModel'));
+    bg = img;
 }
 function imageDiff(background, image) {
     var result = RGBImage_1.RGBImage.fromDimensions(image.getWidth(), image.getHeight());
@@ -100,18 +101,53 @@ function imageDiff(background, image) {
     }
     return result;
 }
+function getBackground(image, backgroundModel, threshold) {
+    backgroundModel = Vision.greyScale(backgroundModel);
+    var imageGreyscale = Vision.greyScale(image);
+    var background = RGBImage_1.RGBImage.fromDimensions(image.getWidth(), image.getHeight());
+    for (var x = 0; x < image.getWidth(); x++) {
+        for (var y = 0; y < image.getHeight(); y++) {
+            var diff = Math.abs(imageGreyscale.r[x][y] - backgroundModel.r[x][y]);
+            if (diff < threshold) {
+                background.r[x][y] = image.r[x][y];
+                background.g[x][y] = image.g[x][y];
+                background.b[x][y] = image.b[x][y];
+            }
+        }
+    }
+    return background;
+}
+function getForeground(image, backgroundModel, threshold) {
+    backgroundModel = Vision.greyScale(backgroundModel);
+    var imageGreyscale = Vision.greyScale(image);
+    var foreground = RGBImage_1.RGBImage.fromDimensions(image.getWidth(), image.getHeight());
+    for (var x = 0; x < image.getWidth(); x++) {
+        for (var y = 0; y < image.getHeight(); y++) {
+            var diff = Math.abs(imageGreyscale.r[x][y] - backgroundModel.r[x][y]);
+            if (diff > threshold) {
+                foreground.r[x][y] = image.r[x][y];
+                foreground.g[x][y] = image.g[x][y];
+                foreground.b[x][y] = image.b[x][y];
+            }
+        }
+    }
+    return foreground;
+}
 function computeFrame() {
     var inputImage = Vision.getImageFromVideo(document.getElementById('webcam'), document.getElementById('camfeed'));
-    var outputImage = imageDiff(background, inputImage);
+    var outputImage = imageDiff(inputImage, bg);
     outputImage.draw(document.getElementById('diff'));
+    outputImage = getBackground(inputImage, bg, threshold);
+    outputImage.draw(document.getElementById('background'));
+    outputImage = getForeground(inputImage, bg, threshold);
+    outputImage.draw(document.getElementById('foreground'));
     if (animating) {
         requestAnimationFrame(computeFrame);
     }
 }
-Vision.initCamera();
 document.getElementById('startBtn').addEventListener('click', function (event) {
     animating = true;
-    if (background == undefined) {
+    if (bg == undefined) {
         getBackgroundFrame();
     }
     requestAnimationFrame(computeFrame);
@@ -122,6 +158,10 @@ document.getElementById('stopBtn').addEventListener('click', function (event) {
 document.getElementById('getBackground').addEventListener('click', function (event) {
     getBackgroundFrame();
 });
+document.getElementById('threshold').addEventListener('change', function (event) {
+    threshold = +this.value;
+});
+Vision.initCamera();
 
 },{"../RGBImage":1,"../vision":3}],3:[function(require,module,exports){
 "use strict";
--- src/entry/bgsub1.ts
+++ src/entry/bgsub1.ts
@@ -1,12 +1,13 @@
 import * as Vision from '../vision';
 import {RGBImage} from '../RGBImage';
 
-let background:RGBImage;
+let bg:RGBImage;
 let animating = false;
+let threshold = 80;
 
 function getBackgroundFrame() {
-    let img = Vision.getImageFromVideo(document.getElementById('webcam') as HTMLVideoElement, document.getElementById('background') as HTMLCanvasElement);
-    background = img;
+    let img = Vision.getImageFromVideo(document.getElementById('webcam') as HTMLVideoElement, document.getElementById('backgroundModel') as HTMLCanvasElement);
+    bg = img;
 }
 
 function imageDiff(background: RGBImage, image: RGBImage): RGBImage {
@@ -17,7 +18,6 @@ function imageDiff(background: RGBImage, image: RGBImage): RGBImage {
             let rdiff = Math.abs(image.r[x][y] - background.r[x][y]);
             let gdiff = Math.abs(image.g[x][y] - background.g[x][y]);
             let bdiff = Math.abs(image.b[x][y] - background.b[x][y]);
-
             result.r[x][y] = rdiff;
             result.g[x][y] = gdiff;
             result.b[x][y] = bdiff;
@@ -27,22 +27,61 @@ function imageDiff(background: RGBImage, image: RGBImage): RGBImage {
     return result;
 }
 
+function getBackground(image: RGBImage, backgroundModel: RGBImage, threshold: number) {
+    backgroundModel = Vision.greyScale(backgroundModel);
+    let imageGreyscale = Vision.greyScale(image);
+    let background = RGBImage.fromDimensions(image.getWidth(), image.getHeight());
+
+    for (let x = 0; x < image.getWidth(); x++) {
+        for (let y = 0; y < image.getHeight(); y++) {
+            let diff = Math.abs(imageGreyscale.r[x][y] - backgroundModel.r[x][y]);
+            if (diff < threshold) {
+                background.r[x][y] = image.r[x][y];
+                background.g[x][y] = image.g[x][y];
+                background.b[x][y] = image.b[x][y];
+            }
+        }
+    }
+    return background;
+}
+
+function getForeground(image: RGBImage, backgroundModel: RGBImage, threshold: number) {
+    backgroundModel = Vision.greyScale(backgroundModel);
+    let imageGreyscale = Vision.greyScale(image);
+    let foreground = RGBImage.fromDimensions(image.getWidth(), image.getHeight());
+
+    for (let x = 0; x < image.getWidth(); x++) {
+        for (let y = 0; y < image.getHeight(); y++) {
+            let diff = Math.abs(imageGreyscale.r[x][y] - backgroundModel.r[x][y]);
+            if (diff > threshold) {
+                foreground.r[x][y] = image.r[x][y];
+                foreground.g[x][y] = image.g[x][y];
+                foreground.b[x][y] = image.b[x][y];
+            }
+        }
+    }
+
+    return foreground;
+}
+
 function computeFrame() {
     let inputImage = Vision.getImageFromVideo(document.getElementById('webcam') as HTMLVideoElement, document.getElementById('camfeed') as HTMLCanvasElement);
 
-    let outputImage = imageDiff(background, inputImage);
+    let outputImage = imageDiff(inputImage, bg);
     outputImage.draw(document.getElementById('diff') as HTMLCanvasElement);
+    outputImage = getBackground(inputImage, bg, threshold);
+    outputImage.draw(document.getElementById('background') as HTMLCanvasElement);
+    outputImage = getForeground(inputImage, bg, threshold);
+    outputImage.draw(document.getElementById('foreground') as HTMLCanvasElement);
 
     if (animating) {
         requestAnimationFrame(computeFrame);
     }
 }
 
-Vision.initCamera();
-
 document.getElementById('startBtn').addEventListener('click', function(event) {
     animating = true;
-    if (background == undefined) {
+    if (bg == undefined) {
         getBackgroundFrame();
     }
     requestAnimationFrame(computeFrame);
@@ -55,3 +94,9 @@ document.getElementById('stopBtn').addEventListener('click', function(event) {
 document.getElementById('getBackground').addEventListener('click', function(event) {
     getBackgroundFrame();
 });
+
+(document.getElementById('threshold') as HTMLInputElement).addEventListener('change', function(event) {
+    threshold = +this.value;
+})
+
+Vision.initCamera();
